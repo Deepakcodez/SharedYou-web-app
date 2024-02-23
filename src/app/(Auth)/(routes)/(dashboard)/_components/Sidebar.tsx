d1f7b8ea10d8a2b1507d57a7931da8098cdbeb59
Deepak <deepak2603dk@gmail.com>
2024-02-24 04:33:05 +0530

    side bar

--- src/app/(Auth)/(routes)/(dashboard)/_components/Sidebar.tsx
+++ src/app/(Auth)/(routes)/(dashboard)/_components/Sidebar.tsx
@@ -1,31 +1,41 @@
-import { FolderKanban, Upload } from "lucide-react";
+import { FolderKanban,  Upload } from "lucide-react";
 import React, { Fragment } from "react";
-
+import Link from 'next/link'
 export const Sidebar = () => {
-  const menu = [
+    type MenuItem = {
+        title: string;
+        icon: React.ReactNode;
+        navigate : string
+      };
+  const menu: MenuItem[]  = [
     {
       title: "Upload",
       icon: <Upload />,
+      navigate : '/upload'
     },
     {
       title: "Files",
       icon: <FolderKanban />,
+      navigate : '/files'
     },
   ];
   return (
-    <div className="h-screen w-[10rem] bg-white text-black p-2 ">
+    <div className="h-screen w-[15rem] bg-white text-black  ">
         <h1 className="border-b border-black/25">
 
       Logo
         </h1>
       {/* menu  */}
-      <div className="menu p-4 pt-8">
+      <div className="menu  pt-8">
         {menu.map((item, index) => (
           <Fragment key={index}>
-            <div className=" flex  gap-4 pt-4 ">
+            <Link  href={item.navigate}   className=" flex  gap-4 pt-2 hover:bg-gray-200 h-14 items-center ">
+                <h1 className="text-gray-500 ps-10 ">
                 {item.icon}
-              <h1>{item.title}</h1>
-            </div>
+
+                </h1>
+              <h1 className="text-gray-500 ">{item.title}</h1>
+            </Link>
           </Fragment>
         ))}
       </div>
